06a1cc81b059c0f42a78b391339152e17ec02683
ESN <nicolas.esseiva@gmail.com>
2022-10-12 11:02:31 +0200

    Exo 7 advanced

--- Exos/S2/Exo7.m
+++ Exos/S2/Exo7.m
@@ -2,7 +2,7 @@
 
 m = 50; % les 50 valeurs de x
 x = linspace(-1, 1, m); k = 0;
-
+AllErr = {0, 0, 0, 0};
 for n = [4 8 16 20]
     for i = 1:m
         y(i) = mon_exp(x(i), n);
@@ -10,8 +10,21 @@ for n = [4 8 16 20]
     % err_rel = (calc-reel)/reel
     ErrRel = (y./exp(x)) -1;
     k = k + 1;
-    subplot(2, 2, k)
+    AllErr(k) = {ErrRel};
+    subplot(3, 2, k)
     plot(ErrRel);
     title(n)
     %ylim([-5e-3 20e-3]); % Pour avoir le meme axe y sur tous les graphes
 end
+
+subplot(3, 2, [5, 6])
+plot(AllErr{1});
+hold on;
+plot(AllErr{2});
+hold on;
+plot(AllErr{3});
+hold on;
+plot(AllErr{4});
+title("All errors on one graph");
+legend(["n=4","n=8","n=16","n=20"])
+
