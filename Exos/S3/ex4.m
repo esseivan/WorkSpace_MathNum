42fd5578b75b86b76e160d948d83b52d9250b954
EsseivaN <nicolas.esseiva@gmail.com>
2022-10-25 16:49:43 +0200

    Ex 4

---
+++ Exos/S3/ex4.m
@@ -0,0 +1,16 @@
+% Ex 4
+
+
+f1 = @(x) exp(x) - 4*x;
+f2 = @(x) x - tan(x);
+f3 = @(x) 1/2 * x * exp(x) - 2 * x^2;
+
+s11 = fzero(f1, 0)
+s12 = fzero(f1, 2)
+
+s21 = fzero(f2, 7)
+%s22 = fzero(f2, 10) % ne fonctionne pas
+s22 = fzero(f2, 10.5)
+
+s31 = fzero(f1, -1)
+s32 = fzero(f1, 2)
