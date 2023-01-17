3d73589654c27f5e5fa11293b480cb9af6534612
ESN <nicolas.esseiva@gmail.com>
2023-01-17 16:51:14 +0100

    Update ex5.m

--- ch6 ed/S6/ex5.m
+++ ch6 ed/S6/ex5.m
@@ -1,4 +1,4 @@
-clear;clc;
+clear;clc;close all;
 
 % ED : (ordre t,y important !)
 dy = @(t, y) y - 2*t;
@@ -10,5 +10,7 @@ y0 = 0;
 
 tspan = [t0, t0+n*h];
 [t, y] = Euler(dy, tspan, y0, h)
+[t_005, y_005] = Euler(dy, tspan, y0, h/10)
 
-plot(t, y, 'o');
+plot(t, y, 'o-', t_005, y_005, 'x-');
+legend("h = 0.5","h = 0.05");
